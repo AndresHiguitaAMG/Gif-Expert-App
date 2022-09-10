c3c12db6da1a80f49f6249a78eee1ea77b039121
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-09 23:34:54 -0500

    new changes

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const AddCategory = ({ onNewCategory }) => {
+    const [ inputValue, setInputValue ] = useState("");
+    
+    const handleOnInputChange = e => {
+        setInputValue(e.target.value);
+    };
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+        onNewCategory(inputValue.trim());
+        setInputValue("");
+    };
+
+    return (
+        <form onSubmit={handleOnSubmit}>
+            <input
+                type={'text'}
+                placeholder={'Buscar gifs'}
+                value={inputValue}
+                onChange={handleOnInputChange}
+            />
+            <button type={'submit'}>Agregar</button>
+        </form>
+  );
+};
+
+export default AddCategory;
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
+
+const GifGrid = ({ category }) => {
+  const [ counter, setCounter ] = useState(10)
+  
+  useEffect(() => {
+    getGifs(category);
+  }, [])
+
+  return (
+    <>
+        <h3>{category}</h3>
+        <h3>{counter}</h3>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
+    </>
+  );
+};
+
+export default GifGrid;
