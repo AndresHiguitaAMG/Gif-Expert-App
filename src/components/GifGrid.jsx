a2562a3b997da6fa34d5841c5c0eefbe4b4b3fee
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-12 14:26:42 -0500

    changes in exports and end of application

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,15 @@
 import useFetchGifs from '../hooks/useFetchGifs';
-import GifItem from './GifItem';
+import { GifItem } from './GifItem';
 
-const GifGrid = ({ category }) => {
+export const GifGrid = ({ category }) => {
   const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
         <h3>{category}</h3>
+        {
+          isLoading && (<h2>Loading...</h2>)
+        }
         <div className='card-grid'>
           {
             images.map((i) => (
@@ -20,5 +23,3 @@ const GifGrid = ({ category }) => {
     </>
   );
 };
-
-export default GifGrid;
--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,10 +1,8 @@
-const GifItem = ({ url, title }) => {
+export const GifItem = ({ url, title }) => {
   return (
     <div className="card">
         <img src={url} alt={title}/>
         <p>{title}</p>
     </div>
   )
-}
-
-export default GifItem;
+};
