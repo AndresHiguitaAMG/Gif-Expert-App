0c679aa30b58a40fe176b406857cd90cbdfdfa78
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-15 10:54:59 -0500

    more tests on the GIFItem component

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem"
 
 describe('Prubeas en <GifItem', () => {
@@ -8,5 +8,21 @@ describe('Prubeas en <GifItem', () => {
         const { container } = render(<GifItem url={url} title={title} />);
 
         expect(container).toMatchSnapshot();
+    });
+
+    test('debe mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem url={url} title={title} />);
+        screen.debug();
+
+        // expect(screen.getByRole('img').src).toBe(url);
+        // expect(screen.getByRole('img').alt).toBe(title);
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('debe de mostrar el titulo en el componente', () => {
+        render(<GifItem url={url} title={title} />);
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });
