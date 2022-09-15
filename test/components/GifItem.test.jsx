00b338f39aa9dccc0bfd560a3f1f2a955fba3200
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-14 23:40:43 -0500

    new changes

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -3,8 +3,8 @@ import { GifItem } from "../../src/components/GifItem"
 
 describe('Prubeas en <GifItem', () => {
     test('debe hacer match con el snapshot', () => {
-        const url = url;
-        const title = title;
+        const url = 'https://giphy.com/clips/cameronpaulsmith-bH03afFirwWm0Qn6wo';
+        const title = "Pikachu";
         const { container } = render(<GifItem url={url} title={title} />);
 
         expect(container).toMatchSnapshot();
