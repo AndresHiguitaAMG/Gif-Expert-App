083877c661005167bb1ea32cb74267113d647864
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-19 13:36:26 -0500

    test in AddCategory component

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -1,9 +1,12 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { AddCategory } from '../../src/components/AddCategory';
 
 describe('Pruebas en <AddCategory />', () => {
     test('debe cambiar el valor de la caja de texto', () => {
         render(<AddCategory onNewCategory={() => {}}/>);
-        screen.debug();
-    })
-})
+        const input = screen.getByRole('textbox');
+        fireEvent.input(input, {target: {value: 'Saitama'}});
+
+        expect(input.value).toBe('Saitama');
+    });
+});
