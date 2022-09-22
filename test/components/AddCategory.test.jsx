4bdcb6b23026e568ca31caa0e40822abebed7771
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-21 20:36:43 -0500

    changes 21

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from '@testing-library/react';
 import { AddCategory } from '../../src/components/AddCategory';
 
 describe('Pruebas en <AddCategory />', () => {
--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react"
-import { GifItem } from "../../src/components/GifItem"
+import { render, screen } from '@testing-library/react';
+import { GifItem } from '../../src/components/GifItem';
 
 describe('Prubeas en <GifItem', () => {
     const url = 'https://giphy.com/clips/cameronpaulsmith-bH03afFirwWm0Qn6wo';
